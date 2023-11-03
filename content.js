9fb645a7652083d3d8eeaa6bed49c64754d5a487
IcramDoku <107223903+IcramDoku@users.noreply.github.com>
2023-11-02 21:14:59 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,2 +1,17 @@
 // content.js
-// Your logic to identify and hide YouTube Shorts elements goes here
+
+// Function to identify and hide YouTube Shorts elements
+function hideYouTubeShorts() {
+  // Replace 'your-selector-for-Shorts' with the actual CSS selector
+  const shortsElements = document.querySelectorAll('#dismissible');
+
+  shortsElements.forEach((element) => {
+    element.style.display = 'none'; // Hide the element
+    // OR
+    // element.remove(); // Remove the element from the DOM
+  });
+}
+
+// Call the hideYouTubeShorts function to start hiding Shorts
+hideYouTubeShorts();
+
