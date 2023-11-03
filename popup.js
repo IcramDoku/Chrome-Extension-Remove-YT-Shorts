2e9d54baf0f3d8937c37726ba7d6483c09aafe56
IcramDoku <107223903+IcramDoku@users.noreply.github.com>
2023-11-02 21:12:01 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,14 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
   const removeShortsCheckbox = document.getElementById("removeShorts");
 
+  // Function to toggle the Shorts remover on/off
+  function toggleShortsRemover(isEnabled) {
+    if (isEnabled) {
+      removeYouTubeShorts(); // Call the function to remove Shorts
+    } else {
+      restoreYouTubeShorts(); // Call the function to restore Shorts if needed
+    }
+  }
+
+  // Function to remove YouTube Shorts
+  function removeYouTubeShorts() {
+    // Replace 'your-selector-for-Shorts' with the actual CSS selector
+    const shortsElements = document.querySelectorAll('#dismissible');
+
+    shortsElements.forEach((element) => {
+      element.style.display = 'none'; // Hide the element
+      // OR
+      // element.remove(); // Remove the element from the DOM
+    });
+  }
+
+  // Function to restore YouTube Shorts if needed
+  function restoreYouTubeShorts() {
+    // Implement logic to restore Shorts elements here
+  }
+
+  // Initialize the checkbox based on the user's stored preference
+  chrome.storage.local.get("isEnabled", function (result) {
+    removeShortsCheckbox.checked = result.isEnabled || false;
+    toggleShortsRemover(result.isEnabled || false);
+  });
+
   removeShortsCheckbox.addEventListener("change", function () {
     const isEnabled = removeShortsCheckbox.checked;
 
-    // Logic to hide or remove YouTube Shorts elements
-    if (isEnabled) {
-      // Implement the logic here
-    } else {
-      // Restore Shorts elements if needed
-    }
+    // Save the user's preference in local storage
+    chrome.storage.local.set({ isEnabled });
+
+    // Toggle the Shorts remover
+    toggleShortsRemover(isEnabled);
   });
 });
+
